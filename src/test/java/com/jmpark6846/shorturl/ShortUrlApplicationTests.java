6a6085c62dc28bcdd0b94ae3751b85923cb93084
jmpark6846 <jmpark6846@naver.com>
2025-11-06 15:59:44 +0900

    first commit

---
+++ src/test/java/com/jmpark6846/shorturl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmpark6846.shorturl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
